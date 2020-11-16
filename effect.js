9e74bf343a254143b101743f6a2c3efbc7cac8d3
muhammadalfi-sy <74496895+muhammadalfi-sy@users.noreply.github.com>
2020-11-16 08:37:49 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -10,9 +10,9 @@ $('document').ready(function(){
 			$('#b22').animate({top:240, left: vw-350},500);
 			$('#b33').animate({top:240, left: vw-250},500);
 			$('#b44').animate({top:240, left: vw-150},500);
-			$('#b55').animate({top:240, left: vw+0},500);
-			$('#b66').animate({top:240, left: vw+50},500);
-			$('#b77').animate({top:240, left: vw+100},500);
+			$('#b55').animate({top:240, left: vw-50},500);
+			$('#b66').animate({top:240, left: vw+0},500);
+			$('#b77').animate({top:240, left: vw+50},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -153,9 +153,9 @@ $('document').ready(function(){
 		$('#b22').animate({top:240, left: vw-350},500);
 		$('#b33').animate({top:240, left: vw-250},500);
 		$('#b44').animate({top:240, left: vw-150},500);
-		$('#b55').animate({top:240, left: vw+0},500);
-		$('#b66').animate({top:240, left: vw+50},500);
-		$('#b77').animate({top:240, left: vw+100},500);
+		$('#b55').animate({top:240, left: vw-50},500);
+		$('#b66').animate({top:240, left: vw+0},500);
+		$('#b77').animate({top:240, left: vw+50},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(4000);
 		$(this).fadeOut('slow').delay(4000).promise().done(function(){
