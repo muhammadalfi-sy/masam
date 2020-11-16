22e703eb8fbded6eca7253bfc843dab77dfa4fc5
muhammadalfi-sy <74496895+muhammadalfi-sy@users.noreply.github.com>
2020-11-16 08:32:45 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -6,14 +6,13 @@ $('document').ready(function(){
 		var vw;
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-400},500);
-			$('#b22').animate({top:240, left: vw-300},500);
-			$('#b33').animate({top:240, left: vw-200},500);
-			$('#b44').animate({top:240, left: vw-100},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+100},500);
-			$('#b77').animate({top:240, left: vw+200},500);
+			$('#b11').animate({top:240, left: vw-450},500);
+			$('#b22').animate({top:240, left: vw-350},500);
+			$('#b33').animate({top:240, left: vw-250},500);
+			$('#b44').animate({top:240, left: vw-150},500);
+			$('#b55').animate({top:240, left: vw+0},500);
+			$('#b66').animate({top:240, left: vw+50},500);
+			$('#b77').animate({top:240, left: vw+100},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -150,13 +149,13 @@ $('document').ready(function(){
 		$('#b5').attr('id','b55')
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-400},500);
-		$('#b22').animate({top:240, left: vw-300},500);
-		$('#b33').animate({top:240, left: vw-200},500);
-		$('#b44').animate({top:240, left: vw-100},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+100},500);
-		$('#b77').animate({top:240, left: vw+200},500);
+		$('#b11').animate({top:240, left: vw-450},500);
+		$('#b22').animate({top:240, left: vw-350},500);
+		$('#b33').animate({top:240, left: vw-250},500);
+		$('#b44').animate({top:240, left: vw-150},500);
+		$('#b55').animate({top:240, left: vw+0},500);
+		$('#b66').animate({top:240, left: vw+50},500);
+		$('#b77').animate({top:240, left: vw+100},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(4000);
 		$(this).fadeOut('slow').delay(4000).promise().done(function(){
